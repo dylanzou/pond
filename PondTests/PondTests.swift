6a9c76168e02ba106348ce4bcd327e6a28245a85
Dylan Zou <dylanzou3@gmail.com>
2017-08-30 15:31:00 -0700

    asdf

---
+++ PondTests/PondTests.swift
@@ -0,0 +1,36 @@
+//
+//  PondTests.swift
+//  PondTests
+//
+//  Created by Dylan Zou on 8/29/17.
+//  Copyright © 2017 Dylan Zou. All rights reserved.
+//
+
+import XCTest
+@testable import Pond
+
+class PondTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
